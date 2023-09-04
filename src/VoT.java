0912bfddad5dca49d9a50ad71ba57b3bfc58a45a
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2023-09-04 09:23:14 +0200

    Integers

--- src/VoT.java
+++ src/VoT.java
@@ -2,7 +2,10 @@ public class VoT {
     public static void main(String[] args) throws Exception {
         System.out.println("Uppgift A");
 
-        
+        int tal1 = 23;
+        int tal2 = 45;
+        int summa;
+        int produkt;
 
 
     }
